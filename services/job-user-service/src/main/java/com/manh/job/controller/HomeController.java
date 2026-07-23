14899615af9de889baf879e1211475144166c20b
manhtranduc <manhtranduc0202@gmail.com>
2026-07-23 15:40:55 +0700

    first commit

---
+++ services/job-user-service/src/main/java/com/manh/job/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.manh.job.controller;
+
+import com.zosh.job.domain.UserRole;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping()
+    public  String HomeController () {
+        return "Job portal user service" + UserRole.ROLE_ADMIN;
+    }
+}
---
+++ services/job-user-service/src/test/java/com/manh/job/JobUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manh.job;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
